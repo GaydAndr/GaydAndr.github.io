71ee149e783d8426b55a98e6c682ea21c1a6b416
GaydAndr <62076338+GaydAndr@users.noreply.github.com>
2020-11-29 02:30:08 +0200

    Add files via upload

---
+++ Futbolki/js/main.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById('btn');
+let menu = document.getElementById('menu');
+btn.onclick = function () {
+	menu.classList.toggle("active")
+};
+
+catalog__btn.onclick = function () {
+	nav.classList.toggle('active')
+}
+
+// $(document).ready(function(){
+// 	$("catalog__btn").click(function () {
+// 		$("nav").classList.toggle('active')
+// 	});
+// });
