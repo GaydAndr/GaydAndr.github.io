c5b0898db13bab071ecfe0d4e3101c109555f53d
GaydAndr <62076338+GaydAndr@users.noreply.github.com>
2020-12-10 17:07:31 +0200

    Update main.js

--- Mr-snoopy/js/main.js
+++ Mr-snoopy/js/main.js
@@ -1,3 +1,8 @@
 ask_btn.onclick = function () {
 	ask_body.classList.toggle('active')
+	arrow_d.classList.toggle('active')
+}
+burger.onclick = function () {
+	menu.classList.toggle('active')
+	burger.classList.toggle('active')
 }
