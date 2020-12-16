49e7e4439c853fa2cf1b1e368f5dfa3a217036b6
GaydAndr <62076338+GaydAndr@users.noreply.github.com>
2020-12-16 21:07:07 +0200

    Update script.js

--- JS_basic/js/script.js
+++ JS_basic/js/script.js
@@ -1,10 +1,10 @@
-let first = 10;
-let second = 20;
+let first = 10,
+    second = 20;
 let result = +first + +second;
 document.write("Результат: " + result);
 
-let user_name = prompt("Вкажіть Ваше ім'я: ");
-let age = prompt("Вкажіть Ваш вік: ");
+let user_name = prompt("Вкажіть Ваше ім'я: "),
+    age = prompt("Вкажіть Ваш вік: ");
 alert("Hello, " + user_name + "! You are " + age + " years old.")
 
 
@@ -13,8 +13,8 @@ document.write("Вік: " + age);
 
 
 
-let number_1 = prompt("Вкажіть перше число: ");
-let number_2 = prompt("Вкажіть друге число: ");
+let number_1 = prompt("Вкажіть перше число: "),
+    number_2 = prompt("Вкажіть друге число: ");
 
 if (number_1 > number_2) {
 	alert('Перше число більше -- ' + number_1 )
