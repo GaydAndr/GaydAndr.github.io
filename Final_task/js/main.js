fbf62bbedc4ba1b9248c1daa818228760124d77f
andrii_h <galgayand@gmail.com>
2021-01-19 19:28:49 +0200

    .

--- Final_task/js/main.js
+++ Final_task/js/main.js
@@ -1,14 +1,37 @@
 const lock = document.querySelector('body');
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
-const menuBtn = document.querySelectorAll('.menuBtn');
-let menuOpen = false;
 
-burger.onclick = function() {
-  menu.classList.toggle("active");
-  burger.classList.toggle("active");
-  lock.classList.toggle("lock");
-  return menuOpen = true;
+$(document).ready(function() {
+  $('#burger').click(function() {
+    menu.classList.toggle("active");
+    burger.classList.toggle("active");
+    lock.classList.toggle("lock");
+  });
+
+  $('.js-btn').on('click', false);
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+});
+
+document.querySelectorAll('.js-btn').forEach(e => {
+  e.onclick = function(event) {
+    let front = event.target.parentNode.parentNode.querySelector('.front');
+    let back = event.target.parentNode.parentNode.querySelector('.back');
+    showBtn(front, back);
+  }
+});
+
+
+let showBtn = function(front, back) {
+  front.classList.toggle("hidden");
+  back.classList.toggle("hidden");
 };
 
 function slowScroll(id) {
@@ -16,9 +39,8 @@ function slowScroll(id) {
   offset = offset.substring(0, offset.length - 2);
   $('html, body').animate({
     scrollTop: $(id).offset().top - (+offset + 5)
-  }, 500);
+  }, 700);
   closeMenu();
-  return menuOpen = false;
 };
 
 function closeMenu() {
