2e80300f9bbff230a1b2b07fa74939ac8062361e
andrii_h <galgayand@gmail.com>
2021-01-20 16:24:19 +0200

    Update main.js

--- Final_task/js/main.js
+++ Final_task/js/main.js
@@ -48,10 +48,10 @@ function closeMenu() {
 let slideIndex = 1;
 const slides1 = document.getElementsByClassName('mySlides1');
 const dots1 = document.getElementsByClassName('my-dot1');
-const slides2 = document.getElementsByClassName('mySlides2');
-const dots2 = document.getElementsByClassName('dot2');
+// const slides2 = document.getElementsByClassName('mySlides2');
+// const dots2 = document.getElementsByClassName('dot2');
 showSlidesSmall();
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
 function showSlidesSmall() {
   let i;
@@ -71,28 +71,28 @@ function showSlidesSmall() {
   setTimeout(showSlidesSmall, 3000);
 }
 
-function showSlides(n) {
-  let i;
-  if (n > slides2.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides2.length;
-  }
-  for (i = 0; i < slides2.length; i++) {
-    slides2[i].style.display = 'none';
-  }
-  for (i = 0; i < dots2.length; i++) {
-    dots2[i].className = dots2[i].className.replace(' active', '');
-  }
-  slides2[slideIndex - 1].style.display = 'block';
-  dots2[slideIndex - 1].className += ' active';
-}
+// function showSlides(n) {
+//   let i;
+//   if (n > slides2.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides2.length;
+//   }
+//   for (i = 0; i < slides2.length; i++) {
+//     slides2[i].style.display = 'none';
+//   }
+//   for (i = 0; i < dots2.length; i++) {
+//     dots2[i].className = dots2[i].className.replace(' active', '');
+//   }
+//   slides2[slideIndex - 1].style.display = 'block';
+//   dots2[slideIndex - 1].className += ' active';
+// }
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
