b61c171dfb22308183d5527a138575d4cf7144b9
andrii_h <galgayand@gmail.com>
2021-01-20 03:23:57 +0200

    .

--- Final_task/js/main.js
+++ Final_task/js/main.js
@@ -1,16 +1,13 @@
 const lock = document.querySelector('body');
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
-
 $(document).ready(function() {
   $('#burger').click(function() {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
     lock.classList.toggle('lock');
   });
-
   $('.js-btn').on('click', false);
-
   $(window).scroll(function() {
     if ($(this).scrollTop() > 500) {
       $('.scrollup').fadeIn();
@@ -19,7 +16,6 @@ $(document).ready(function() {
     }
   });
 });
-
 document.querySelectorAll('.js-btn').forEach((e) => {
   e.onclick = function(event) {
     let front = event.target.parentNode.parentNode.querySelector('.front');
@@ -27,7 +23,6 @@ document.querySelectorAll('.js-btn').forEach((e) => {
     showBtn(front, back);
   };
 });
-
 let showBtn = function(front, back) {
   front.classList.toggle('hidden');
   back.classList.toggle('hidden');
